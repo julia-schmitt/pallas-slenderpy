1cbd360fcc99b79a4ff294cfbeeadd3ea083736e
Schmitt Julia <julia.schmitt@mews-labs.com>
2025-10-10 14:21:48 +0200

    separation of BC and correction of matrix for second derivative

---
+++ src/slenderpy/future/beam/fd_utils.py
@@ -0,0 +1,145 @@
+from typing import Tuple, Optional
+
+import numpy as np
+import scipy as sp 
+
+
+def second_derivative(n: int, ds: float) -> np.ndarray[[float], [float]]:
+    # centered scheme, the first and last line have to be completed with BC 
+
+    dinf = +1.0 * np.ones((n - 1,))/ds**2
+    diag = -2.0 * np.ones((n ,))/ds**2
+    dsup = +1.0 * np.ones((n - 1,))/ds**2
+
+    dinf[-1] = 0
+    diag[0] = 0; diag[-1] = 0
+    dsup[0] = 0
+
+    res = sp.sparse.diags([dinf, diag, dsup], [-1, 0, 1])
+
+    return res
+
+
+def fourth_derivative(n: int, ds: float) -> np.ndarray[[float], [float]]:
+    # centered scheme, the two first and two last line have to be completed with BC 
+
+    dinf2 = +1.0 * np.ones((n - 2))/ds**4
+    dinf1 = -4.0 * np.ones((n - 1,))/ds**4
+    diag = +6.0 * np.ones((n ,))/ds**4
+    dsup1 = -4.0 * np.ones((n - 1,))/ds**4
+    dsup2 = +1.0 * np.ones((n - 2,))/ds**4
+
+    dinf2[-1] = 0; dinf2[-2] = 0
+    dinf1[-1] = 0; dinf1[-2] = 0; dinf1[0] = 0
+    diag[0] = 0; diag[1] = 0; diag[-2] = 0; diag[-1] = 0
+    dsup1[0] = 0; dsup1[1] = 0; dsup1[-1] = 0
+    dsup2[0] = 0; dsup2[1] = 0
+
+    res = sp.sparse.diags([dinf2, dinf1, diag, dsup1, dsup2], [-2, -1, 0, 1, 2])
+
+    return res
+
+
+
+class BoundaryCondition:
+    """Object to deal with boundary conditions."""
+
+    def __init__(
+        self,
+        order : int, # pour l'instant je traite que le cas 2 et 4 
+        left: Optional[Tuple[Tuple[float, float, float, float],Tuple[float, float, float, float]]] = None,
+        right: Optional[Tuple[Tuple[float, float, float, float],Tuple[float, float, float, float]]] = None,
+    ) -> None:
+        
+        if order == 4:
+            if left is None:
+                left = ((1.0, 0.0, 0.0, 0.0), (0.0, 1.0, 0.0, 0.0))
+
+            if right is None:
+                right = ((1.0, 0.0, 0.0, 0.0), (0.0, 1.0, 0.0, 0.0))
+
+            if (not isinstance(left, (tuple, list))) or (not isinstance(right, (tuple, list))):
+                raise TypeError("Inputs left and right must be list or tuples")
+
+            if len(left) != 2 or len(right) != 2 : 
+                raise ValueError("Need two boundary condition for each extremity")
+
+            
+            if len(left[0]) != 4 or len(left[1]) !=4 or len(right[0]) != 4 or len(right[1]) != 4:
+                raise ValueError("Inputs must have 4 elements for each boundary condition")
+
+            rankL = np.linalg.matrix_rank(left)
+            rankR = np.linalg.matrix_rank(right)
+
+            if rankL < 2:
+                raise ValueError("The left boundary conditions are not linearly independant")
+            
+            if rankR < 2:
+                raise ValueError("The right boundary conditions are not linearly independant")
+            
+
+        if order == 2: 
+            if left is None:
+                left = ((1.0, 0.0, 0.0, 0.0),)
+
+            if right is None:
+                right = ((1.0, 0.0, 0.0, 0.0),)
+
+            if (not isinstance(left, (tuple, list))) or (not isinstance(right, (tuple, list))):
+                raise TypeError("Inputs left and right must be list or tuples")
+
+            if len(left) != 1 or len(right) != 1 : 
+                raise ValueError("Need one boundary condition for each extremity")
+            
+            if len(left[0]) != 4 or len(right[0])  != 4:
+                raise ValueError("Inputs must have 4 elements for each boundary condition")
+
+            rankL = np.linalg.matrix_rank(left)
+            rankR = np.linalg.matrix_rank(right)
+
+            if rankL < 1:
+                raise ValueError("There is no left boundary condition")
+            
+            if rankR < 1:
+                raise ValueError("There is no right boundary condition")
+        
+        self.left = left
+        self.right = right
+        self.order = order
+
+
+    def compute(self, ds: float, n:int)-> Tuple[np.ndarray[[float], [float]], np.ndarray[float]]:
+        a1, b1, c1, d1 = self.left[0]
+        a3, b3, c3, d3 = self.right[0]
+
+        bc_matrix = sp.sparse.lil_matrix((n,n))
+        rhs = np.zeros(n)
+
+        bc_matrix[0, 0] = a1 - b1 / ds + c1 / ds**2
+        bc_matrix[0, 1] = b1 / ds - 2 * c1 / ds**2
+        bc_matrix[0, 2] = c1 / ds**2
+
+        bc_matrix[-2, -1] = a3 + b3 / ds + c3 / ds**2 
+        bc_matrix[-2, -2] = -b3 / ds - 2 * c3 / ds**2 
+        bc_matrix[-2, -3] = c3 / ds**2 
+
+        rhs[0] = d1
+        rhs[-2] = d3
+
+        if self.order == 4:
+            a2, b2, c2, d2 = self.left[1]
+            a4, b4, c4, d4 = self.right[1]
+
+            bc_matrix[1, 0] = a2 - b2 / ds + c2 / ds**2 
+            bc_matrix[1, 1] = b2 / ds - 2 * c2 / ds**2 
+            bc_matrix[1, 2] = c2 / ds**2
+
+            bc_matrix[-1, -1] = a4 + b4 / ds + c4 / ds**2
+            bc_matrix[-1, -2] = -b4 / ds - 2 * c4 / ds**2
+            bc_matrix[-1, -3] = c4 / ds**2
+
+            rhs[-1] = d4
+            rhs[1] = d2
+
+
+        return bc_matrix, rhs
--- src/slenderpy/future/beam/static.py
+++ src/slenderpy/future/beam/static.py
@@ -2,16 +2,29 @@ import numpy as np
 import scipy as sp 
 import matplotlib.pyplot as plt
 
-import FD_utils as FD 
-from slenderpy.beam import Beam 
+import fd_utils as FD 
 
+def clean_matrix(n : int, remove):
+    # renvoie une matrice qui permet d'éliminer les termes polluants les BC 
+    res = sp.sparse.lil_matrix((n,n))
+    remove = remove.todense()
+
+    res[1, 0] -= remove[1,0]
+    res[1, 1] -= remove[1,1]
+    res[1, 2] -= remove[1,2]
+    res[-2, -1] -= remove[-2,-1]
+    res[-2, -2] -= remove[-2,-2]
+    res[-2, -3] -= remove[-2,-3]
+
+    return res
 
 def solve_beam_const(n : int, ds : float, EI: float, H : float, bc:FD.BoundaryCondition):
     ### equation : EI*(d^4/dx^4)*y - H*(d^2/dx^2)*y = F(x) ###
     A4 = EI*FD.fourth_derivative(n, ds)
-    A2 = -H*FD.second_derivative(n, ds)
-    BC, rhs = bc.compute(ds, n, A2)
-    A = A4 + A2 + BC 
+    A2 = -H*FD.second_derivative(n, ds) # si H = 0 D2 est quand même calculé ? 
+    BC, rhs = bc.compute(ds, n)
+    CM = clean_matrix(n, A2)
+    A = A4 + A2 + BC + CM
     sol = sp.sparse.linalg.spsolve(A, rhs)
     return sol
 
@@ -34,16 +47,14 @@ if __name__ == "__main__":
     ds = 1 / (n - 1)
     x = np.linspace(0, 1, n)
 
-    ### solve y" = 0 with BC, equation 1 (E1) ###
+    ### solve y"" = 0 with BC, equation 1 (E1) ###
 
-    t1 = [1, 0, 0, 0]
-    t2 = [0, 1, 0, 1]
-    t3 = [0, 1, 0, 2]
-    t4 = [1, 0, 0, 1]
+    left = [[1, 0, 0, 0], [0, 1, 0, 1]]
+    right = [[0, 1, 0, 2], [1, 0, 0, 1]] 
     EI = 1
     H = 1
 
-    bc = FD.BoundaryCondition(t1,t2,t3,t4)
+    bc = FD.BoundaryCondition(4, left, right)
     function1 = exact_solutionE1(x)
     sol1 = solve_beam_const(n,ds,EI,H,bc)
 
@@ -52,14 +63,11 @@ if __name__ == "__main__":
     plt.legend()
     plt.show()
 
-    # ### solve y"" - y" = 0 with BC, equation 2 (E2) ###
+    # # ### solve y"" - y" = 0 with BC, equation 2 (E2) ###
 
-    t1 = [1, 0, 0, 0]
-    t2 = [0, 0, 1, 1]
-    t3 = [1, 0, 0, 0]
-    t4 = [0, 0, 1, 0]
-
-    bc = FD.BoundaryCondition(t1,t2,t3,t4)
+    left = [[1, 0, 0, 0], [0, 0, 1, 1]]
+    right = [[1, 0, 0, 0],[0, 0, 1, 0]]
+    bc = FD.BoundaryCondition(4,left,right)
     function2 = exact_solutionE2(x)
     sol2 = solve_beam_const(n,ds,EI,H,bc)
 
@@ -67,3 +75,4 @@ if __name__ == "__main__":
     plt.plot(x, sol2, color="orange", label="approx")
     plt.legend()
     plt.show()
+
