bc8d34656251f8c43ab079f05914fb1f07cdc448
Schmitt Julia <julia.schmitt@mews-labs.com>
2025-10-31 16:02:37 +0100

    dynamic ok for 2nd derivative in space with EI constant and approx curvature

--- src/slenderpy/future/beam/dynamic.py
+++ src/slenderpy/future/beam/dynamic.py
@@ -6,10 +6,12 @@ import scipy as sp
 import slenderpy.future.beam.fd_utils as FD
 import slenderpy.future.beam.static as ST
 
+
 def _solve_curvature_approx(
     nb_space: int,
-    nb_time: int,
-    bc: FD.BoundaryCondition,
+    dt: float,
+    bc_y: FD.BoundaryCondition,
+    bc_v: FD.BoundaryCondition,
     lspan: float = 400.0,
     final_time : float = 30.0,
     mass : float = 10., #tout diviser par la masse 
@@ -18,62 +20,72 @@ def _solve_curvature_approx(
     ei_min: float = None,
     critical_curvature = None, 
     force = None, #a modifier 
-    inital_position = None,
+    initial_position = None,
     initial_velocity = None
 ) -> sp.sparse.spmatrix:
+#ajouter l'option de commencer à n'importe quel temps initial 
     """Solve equation of the form : m*(d^2/dt^2)*y + ei*(d^4/dx^4)*y - tension*(d^2/dx^2)*y = rhs."""
 
     ds = lspan / (nb_space - 1)
-    dt = final_time / nb_time
-    dt2 = dt/2.
+    dt2 = dt*0.5
     x = np.linspace(0.,lspan,nb_space)
 
-    t = 0.0
-    y_old = inital_position
-    y_prime_old = initial_velocity
+    # print("rapport = ", dt/ds**2)
 
-    y_all_time = []
+    t = 0.
+    y_old = initial_position(x,t)
+    v_old = initial_velocity(x,t)
+    y_all_time = [y_old]
 
     ei = ei_max
 
     D2 = FD.second_derivative(nb_space, ds)
-    D2 = FD.clean_matrix(bc.order, D2)
-    D4 = FD.fourth_derivative(nb_space, ds)
-    BC, rhs_bc = bc.compute(ds, nb_space)
+    # D2 = FD.clean_matrix(bc_v.order, D2)
+    D4 = FD.fourth_derivative(nb_space , ds)
+    # BC, rhs_bc = bc_v.compute(ds, nb_space)
+    # BC, rhs_bc = bc_y.compute(ds, nb_space)
 
-    for _ in range(100):
-        force_previsous = FD.clean_rhs(bc.order, np.copy(force(x, t)))
-        force_current = FD.clean_rhs(bc.order, np.copy(force(x, t + dt)))
-        y_prime_old = FD.clean_rhs(bc.order, np.copy(y_prime_old))
+    Id = sp.sparse.identity(nb_space)
+    # Id.data[0,0] = 0
+    # Id.data[0,1] = 0
+    # Id.data[0,-1] = 0
+    # Id.data[0,-2] = 0
 
-        equation = ei*D4 - tension*D2
+    K = ei*D4 - tension*D2
+    M = mass*Id
 
-        rhs = mass*y_prime_old + dt2*(force_previsous + force_current - 2*equation@y_old)  - dt2**2*equation@y_prime_old + rhs_bc
-        Id = sp.sparse.identity(nb_space)
-        Id.data[0,0] = 0
-        Id.data[0,1] = 0
-        Id.data[0,-1] = 0
-        Id.data[0,-2] = 0
-        A =  mass*Id + dt2**2*equation + BC
+    for _ in range(final_time):
+        # force_previsous = FD.clean_rhs(bc_v.order, np.copy(force(x, t)))
+        # force_current = FD.clean_rhs(bc_v.order, np.copy(force(x, t + dt)))
+        # v_old = FD.clean_rhs(bc_v.order, np.copy(v_old))
+        # y_old = FD.clean_rhs(bc_y.order, np.copy(y_old))
 
-        y_prime_new = sp.sparse.linalg.spsolve(A,rhs) 
-        y_new = y_old + dt2*(y_prime_old + y_prime_new)
-        y_new[0:2] = y_new[0:2]  
-        y_new[-2::] = y_new[-2::]
+        rhs = (M - dt2**2*K)@v_old + dt2*(force(x,t) + force(x,t + dt)) - dt*K@y_old  #+ rhs_bc
+        A =  M + dt2**2*K  #+ BC
 
-        y_prime_old = y_prime_new
+        v_new = sp.sparse.linalg.spsolve(A,rhs)
+        v_new[0] = 0
+        v_new[-1] = 0
+        # v_new = v_old + dt2*(force_previsous + force_current - K@y_old - K@y_new)/mass
+
+        # v_new[0:2] = np.array([0,0])
+        # v_new[-2::] = np.array([0,0])
+
+        y_new = y_old + dt2*(v_old + v_new)
+        y_new[0] = 1
+        y_new[-1] = 1
+        # y_new[0:2] = np.array([1,1])
+        # y_new[-2::] = np.array([1,1])
+
+        v_old = v_new
         y_old = y_new
         t += dt
 
-        # rhs_bc[0] = -2.*np.pi*np.sin(2*np.pi*t)
-        # rhs_bc[1] = 2*np.pi*np.cos(2*np.pi*t)
-        # rhs_bc[-1] = -2.*np.pi*np.sin(2*np.pi*t - lspan)
-        # rhs_bc[-2] = 2*np.pi*np.cos(2*np.pi*t - lspan)
+        y_all_time.append(y_new)
 
-        y_all_time.append(y_prime_new)
+    return np.array(y_all_time)
 
 
-    return y_all_time
 
 def _solve_curvature_exact(
     nb_space: int,
--- test/test_future-beam-dynamic.py
+++ test/test_future-beam-dynamic.py
@@ -2,69 +2,217 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
-from slenderpy.future.beam.dynamic import _solve_curvature_exact
+from slenderpy.future.beam.dynamic import _solve_curvature_approx, _solve_curvature_exact
+import slenderpy.future.beam.static as ST
 import slenderpy.future.beam.fd_utils as FD
-import slenderpy.beam as bm 
-import slenderpy.simtools as sm 
-import slenderpy.fdm_utils as fdm
 
 
-nb_space = 100
-nb_time = 1
-final_time = 1.94
-dt = final_time/nb_time
-# mass = 1.
-tension = 1.
-ei_max = 1.
-lmin = 0.
-lmax = 1.
-lspan = lmax - lmin 
+def test_dynamic_approx_curvature_static_BC():
+    nb_space = 200
+    dt = 1/10000. 
+    final_time = 10000
+    mass = 1.
+    tension = 1.
+    ei_max = 0.
+    lmin = 0. 
+    lmax = 3.
+    lspan = lmax - lmin 
 
-x = np.linspace(lmin, lmax, nb_space)
+    x = np.linspace(lmin, lmax, nb_space)
 
-def rhs(x,t):
-    return np.cosh(x + t) + ei_max*( -2 / np.cosh(x + t)**2 + 6.0 * np.sinh(x + t)**2 / np.cosh(x + t) ** 4) - tension*np.cosh(x + t)
+    def f(t):
+        return np.cos(t)
 
-def exact(x,t):
-    return np.cosh(x + t)
+    def exact(x,t):
+        return f(t)*x**2*(x-lmax)**2 + 1
+    
+    def exact_time_derivative(x,t):
+        return -np.sin(t)*x**2*(x-lmax)**2
+    
+    def force(x,t):
+        # return -mass*np.cos(t)*x**2*(x-lmax)**2 + ei_max*24.*f(t) - tension*f(t)*(12*x**2 -12*lmax*x + 2*lmax**2)
+        return -mass*np.cos(t)*x**2*(x-lmax)**2 - tension*f(t)*(12*x**2 -12*lmax*x + 2*lmax**2)
+    
 
-def exact_time_derivative(x,t):
-    return np.sinh(x + t)
+    left = [[1, 0, 0, 1], [0, 1, 0, 0]]
+    right = [[1, 0, 0, 1], [0, 1, 0, 0]]
+    bc_y = FD.BoundaryCondition(4, left, right)
+
+    left = [[1, 0, 0, 0], [0, 1, 0, 0]]
+    right = [[1, 0, 0, 0], [0, 1, 0, 0]]
+    bc_v = FD.BoundaryCondition(4, left, right)
+
+    # sol = solve_CN(nb_space = nb_space,dt = dt, lspan = lspan, final_time = final_time, force = force, initial_position = exact, initial_velocity = exact_time_derivative)
+
+    sol = _solve_curvature_approx(nb_space = nb_space, dt = dt, bc_y = bc_y, bc_v = bc_v,
+                                lspan = lspan, final_time = final_time, mass = mass, tension = tension,
+                                ei_max = ei_max, force = force, initial_position = exact, initial_velocity = exact_time_derivative)
+    
+
+    t = 2000
+    
+    # plt.plot(x,exact(x, t*dt),color = 'blue', label = 'Analytical solution') 
+    # plt.plot(x,exact_time_derivative(x,t*dt),color = 'green', label = 'Analytical time derivative solution')
+    # plt.plot(x,sol[t], color = 'orange', label = 'Approximate solution')
+    # plt.legend()
+    # plt.xlim(lmin, lmax)
+    # plt.ylim(-7,7)
+    # plt.show()
+
+    # fig = plt.figure()
+    # line_exact, = plt.plot([], [], color='blue', label = 'Analytical solution')
+    # line_approx, = plt.plot([], [], color='orange', label = 'Approximate solution')
+    # plt.legend()
+    # plt.xlim(lmin, lmax)
+    # plt.ylim(-7,7)
+
+    # def animate(i):
+    #     t = i*dt
+    #     analytical = exact(x,t)
+    #     approx = sol[i]
+    #     line_exact.set_data(x, analytical)
+    #     line_approx.set_data(x,approx)
+    #     return line_exact
+
+    # ani = animation.FuncAnimation(fig,animate,frames = np.arange(1,final_time,100), interval=1)
+    # ani.save("animation.mp4", writer="ffmpeg", fps=30)
+
+    # plt.show()
+
+    analitical_results = np.array([exact(x,i*dt) for i in range(final_time + 1)])
+
+    atol = 1.0e-06
+    rtol = 1.0e-04
+
+    assert np.allclose(analitical_results, sol, atol=atol, rtol=rtol)
 
 
-#checker les conditions au bords avec le temps 
-t = 0.
-
-left = [[1, 0, 0, np.cosh(lmin + t)], [0, 1, 0, np.sinh(lmin + t)]]
-right = [[1, 0, 0, np.cosh(lmax + t)], [0, 1, 0, np.sinh(lmax + t)]]
-bc_y = FD.BoundaryCondition(4, left, right)
-
-sol = _solve_curvature_exact(nb_space = nb_space, nb_time = nb_time, bc = bc_y, 
-                            lspan = lspan, final_time = final_time, tension = tension,
-                            ei_max = ei_max, force = rhs)
 
 
 
-# fig = plt.figure()
-# line_exact, = plt.plot([], [], color='blue')
-# line_approx, = plt.plot([], [], color='orange')
-# plt.xlim(lmin, lmax)
-# plt.ylim(0,10)
+def test_dynamic_approx_curvature():
+    nb_space = 1000
+    nb_time = 100
+    final_time = 10.
+    dt = final_time/nb_time
+    mass = 1.
+    tension = 1.
+    ei_max = 1.
+    lmin = 0. 
+    lmax = 8.
+    lspan = lmax - lmin 
 
-# def animate(i):
-#     t = i*dt
-#     exact = exact_time_derivative(x,t)
-#     approx = sol[i]
-#     line_exact.set_data(x, exact)
-#     line_approx.set_data(x,approx)
-#     return line_exact, line_approx,
+    x = np.linspace(lmin, lmax, nb_space)
 
-# ani = animation.FuncAnimation(fig,animate,frames = nb_time,interval=1)
+    def exact(x,t):
+        return np.cos(2*np.pi*t - x)
 
-# plt.show()
+    def exact_time_derivative(x,t):
+        return -2.*np.pi*np.sin(2*np.pi*t - x)
 
-plt.plot(x,sol[0], color = 'orange')
-plt.plot(x,exact(x,t), color = 'blue')
-# # plt.plot(x,exact_time_derivative(x,t), color = 'green')
-plt.show()
+    def exact_space_derivative(x,t):
+        return np.sin(2*np.pi*t - x)
+    
+    def exact_space_time_derivative(x,t):
+        return 2*np.pi*np.cos(2*np.pi*t - x)
+    
 
+    def force(x,t):
+        return -mass*4.*np.pi**2*exact(x,t) + ei_max*exact(x,t) + tension*exact(x,t) 
+
+    left = [[1, 0, 0, exact_time_derivative(lmin, dt)], [0, 1, 0, exact_time_derivative(lmin, dt)]]
+    right = [[1, 0, 0, exact_space_time_derivative(lmax, dt)], [0, 1, 0, exact_space_time_derivative(lmax, dt)]]
+    bc_y = FD.BoundaryCondition(4, left, right)
+
+    sol = _solve_curvature_approx(nb_space = nb_space, nb_time = nb_time, bc_y = bc_y, bc_v = bc_y,
+                                lspan = lspan, final_time = final_time, tension = tension,
+                                ei_max = ei_max, force = force, inital_position = exact, initial_velocity = exact_time_derivative)
+    
+
+    plt.plot(x,exact(x,1*dt),color = 'blue', label = 'Analytical solution')
+    # plt.plot(x,exact_time_derivative(x,1*dt),color = 'green', label = 'Analytical time derivative solution')
+    plt.plot(x,sol[1], color = 'orange', label = 'Approximate solution')
+    plt.legend()
+    plt.show()
+
+    # fig = plt.figure()
+    # line_exact, = plt.plot([], [], color='blue', label = 'Analytical solution')
+    # line_approx, = plt.plot([], [], color='orange', label = 'Approximate solution')
+    # plt.legend()
+    # plt.xlim(lmin, lmax)
+    # plt.ylim(-2,2)
+
+    # def animate(i):
+    #     t = i*dt
+    #     analytical = exact(x,t + dt)
+    #     approx = sol[i]
+    #     line_exact.set_data(x, analytical)
+    #     line_approx.set_data(x,approx)
+    #     return line_exact
+
+    # ani = animation.FuncAnimation(fig,animate,frames = 1000,interval=1)
+
+    # plt.show()
+
+
+def test_dynamic_exact_curvature():
+    nb_space = 10000
+    nb_time = 10000
+    final_time = 10.
+    dt = final_time/nb_time
+    mass = 1.
+    tension = 1.
+    ei_max = 1.
+    lmin = 0. 
+    lmax = 2.
+    lspan = lmax - lmin 
+    x = np.linspace(lmin, lmax, nb_space)
+
+
+    def rhs(x,t):
+        return np.cosh(x + t) + ei_max*( -2 / np.cosh(x + t)**2 + 6.0 * np.sinh(x + t)**2 / np.cosh(x + t) ** 4) - tension*np.cosh(x + t)
+
+    def exact(x,t):
+        return np.cosh(x + t)
+
+    def exact_time_derivative(x,t):
+        return np.sinh(x + t)
+
+    left = [[1, 0, 0, np.cosh(lmin + dt)], [0, 1, 0, np.sinh(lmin + dt)]]
+    right = [[1, 0, 0, np.cosh(lmax + dt)], [0, 1, 0, np.sinh(lmax + dt)]]
+    bc_y = FD.BoundaryCondition(4, left, right)
+
+    sol_curv = _solve_curvature_exact(nb_space = nb_space, nb_time = nb_time, bc = bc_y, 
+                                lspan = lspan, final_time = final_time, tension = tension,
+                                ei_max = ei_max, force = rhs, inital_position = exact, initial_velocity = exact_time_derivative)
+
+
+    plt.plot(x,sol_curv[2],color = 'orange', label = 'Approximate solution')
+    plt.plot(x,exact(x,3*dt),color = 'blue', label = 'Analytical solution')
+    plt.show()
+
+    # fig = plt.figure()
+    # line_exact, = plt.plot([], [], color='blue')
+    # line_approx, = plt.plot([], [], color='orange')
+    # plt.xlim(lmin, lmax)
+    # plt.ylim(-2,2)
+
+    # def animate(i):
+    #     t = i*dt
+    #     analytical = exact(x,t + dt)
+    #     approx = sol[i]
+    #     line_exact.set_data(x, analytical)
+    #     line_approx.set_data(x,approx)
+    #     return line_exact
+
+    # ani = animation.FuncAnimation(fig,animate,frames = nb_time,interval=1)
+
+    # plt.show()
+
+    
+
+if __name__ == "__main__":
+    # test_scheme_CN()
+    test_dynamic_approx_curvature_static_BC()
+    # test_dynamic_approx_curvature()
+    # test_dynamic_exact_curvature()
